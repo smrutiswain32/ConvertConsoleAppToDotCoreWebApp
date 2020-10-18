07ee1d86db8fe9f4e1b8f0443f951b443fe2f234
smruti swain <smrutiswain35@outlook.com>
2020-10-19 01:02:16 +0530

    Add project files.

---
+++ ConvertConsoleAppToDotNetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConvertConsoleAppToDotNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
