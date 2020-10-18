b233ccdb54e991b18a931f4494ba121f6df2669a
smruti swain <smrutiswain35@outlook.com>
2020-10-19 01:27:03 +0530

    Convert console app to dotnet Core web app

--- ConvertConsoleAppToDotNetCore/Program.cs
+++ ConvertConsoleAppToDotNetCore/Program.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ConvertConsoleAppToDotNetCore
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+       
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+        
     }
 }
---
+++ ConvertConsoleAppToDotNetCore/Startup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConvertConsoleAppToDotNetCore
+{
+    public class Startup
+    {
+
+        public void ConfigurationServices(IServiceCollection service)
+        {
+
+        }
+
+
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoint =>
+            {
+                endpoint.MapGet("/", async conext =>
+                {
+                  await  conext.Response.WriteAsync("Hello from Application");
+                });
+            });
+
+        }
+    }
+}
